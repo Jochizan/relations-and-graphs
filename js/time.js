9f27662a8d214fef6d5de782f6a08aa55b50ec7c
Johan769 <remnyachizot2015@gmail.com>
2020-11-21 12:18:21 -0500

    Mejorando la pagina y reloj añadiendo parte del header

--- js/time.js
+++ js/time.js
@@ -3,7 +3,14 @@ const timeStatus = () => {
   let hora = momentActual.getHours();
   let minuto = momentActual.getMinutes();
   let segundo = momentActual.getSeconds();
-  let mostrar = hora + " : " + minuto + " : " + segundo;
-  document.form_reloj.reloj.value = mostrar;
+  // agregando un 0 si fuese de 1 sola cifra
+  minuto = checkTime(minuto);
+  segundo = checkTime(segundo);
+  document.getElementById('clock').innerHTML = hora + " : " + minuto + " : " + segundo;
+  //document.form_reloj.reloj.value = mostrar;
   setTimeout("timeStatus()", 1000);
 }
+
+const checkTime = (i="") => (i < 10)
+  ? '0' + i
+  : i;
